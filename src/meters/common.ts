78a4cb3676d6e141c0bd4c6593a2bb5eaa8db9da
cody <cevek@ya.ru>
2017-01-23 02:17:33 +0500

    Add ifs metric

---
+++ src/meters/common.ts
@@ -0,0 +1,42 @@
+import {Metric} from '../common/Metric';
+import {perfStart, perfEnd} from '../common/performance';
+
+export class EmptyMetric implements Metric {
+    name = 'empty';
+    timing = Infinity;
+
+    run() {
+        const start = perfStart();
+        let ret = 0;
+        for (let i = 0; i < 1e6; i++) {
+            ret = i;
+        }
+        const dur = perfEnd(start);
+        this.timing = Math.min(this.timing, dur);
+        return ret;
+    }
+}
+
+export namespace ifs {
+    export const name = 'k ? 1 : 0';
+
+    function ifsLoop(N: number, n0: number, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number, n9: number) {
+        let ret = 0;
+        for (let i = 0; i < N; i++) {
+            ret += (n0 ? 1 : 0) + (n1 ? 1 : 0) + (n2 ? 1 : 0) + (n3 ? 1 : 0) + (n4 ? 1 : 0) + (n5 ? 1 : 0) + (n6 ? 1 : 0) + (n7 ? 1 : 0) + (n8 ? 1 : 0) + (n9 ? 1 : 0);
+        }
+        return ret;
+    }
+
+    export function run() {
+        const start = perfStart();
+        ifsLoop(1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        ifsLoop(1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        ifsLoop(1e5, 1, 0, 3, 0, 5, 0, 7, 0, 9, 0);
+        return perfEnd(start);
+    }
+
+    export let dur = Infinity;
+    dur = Math.min(dur, run());
+    dur = Math.min(dur, run());
+}
